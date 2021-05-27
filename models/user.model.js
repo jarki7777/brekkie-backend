16356bbfec1805a7317484405e678ef1a42d6f9e
TheJarki <melendez.jarki@gmail.com>
2021-05-27 13:56:53 +0200

    user model first approach

--- models/user.model.js
+++ models/user.model.js
@@ -1,16 +1,71 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-    name: String,
-    lastName: String,
+    email: {
+        type: username,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function (v) {
+// username can't containg whitespaces must be between 4 and 20 characters,
+// can contain only letters, numbers and any of these non consecutive special characters '-._' 
+                return /^([a-z0-9]|[-._](?![-._])){4,20}$/i.test(v);
+            },
+            message: props => `${props.value} is not a valid email`
+        }
+    },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        validate: {
+            validator: function (v) {
+// email must be a valid email string
+                return /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(v);
+            },
+            message: props => `${props.value} is not a valid email`
+        }
     },
-    phone: Number
+    password: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function (v) {
+// password must be between 8 and 32 characters, contain at least 1 lowercase letter,
+// 1 uppercase letter, 1 number and one of this special characters '#<>$+@$!%*?&'
+                return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!#$%&?¿¡*-+.<>])[A-Za-z\d[!#$%&?¿¡*-+.<>]{8,32}$/gm.test(v);
+            },
+            message: props => `${props.value} is not a valid password`
+        }
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'mod', 'client'],
+        default: 'client'
+    },
+    caloriesGoal: {
+        type: Number,
+        min: 1
+    },
+    inventory: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Inventories'
+    },
+    shoppingList: {
+        type: mongoose.Types.ObjectId,
+        ref: 'ShoppingLists'
+    },
+    favorites: {
+        type: mongoose.Types.ObjectId,
+        ref: 'favorites'
+    },
+    foodLog: {
+        type: mongoose.Types.ObjectId,
+        ref: 'foodLogs'
+    }
 });
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', UserSchema, 'users');
 
 export default User;
