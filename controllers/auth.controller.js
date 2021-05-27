3886ce93c5c3a13b44a472fb901dac00f499c74f
TheJarki <melendez.jarki@gmail.com>
2021-05-27 17:48:44 +0200

    login endpoint

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -7,9 +7,13 @@ export const authController = {
     signUp: async (req, res) => {
         try {
             const { username, email, password } = req.body;
-            const checkEmailAvailable = await User.findOne({ email });
-            const checkUsernameAvailable = await User.findOne({ username });
-            const checkPassword = validatePw(password);
+            const emailNotAvailable = await User.findOne({ email });
+            const usernameNotAvailable = await User.findOne({ username });
+            const checkPw = validatePw(password);
+
+            if (emailNotAvailable) res.status(409).send({ 'message': 'Email already exist' });
+            else if (usernameNotAvailable) res.status(409).send({ 'message': 'Username already exist' });
+            else if (!checkPw) res.status(400).send({ 'message': 'Password does not meet the criteria' });
 
             const userData = {
                 email: email,
@@ -17,16 +21,13 @@ export const authController = {
                 password: bcrypt.hashSync(password, 10)
             }
 
-            if (checkEmailAvailable) res.status(409).send({ 'message': 'Email already exist' });
-            else if (checkUsernameAvailable) res.status(409).send({ 'message': 'Username already exist' });
-            else if (!checkPassword) res.status(400).send({ 'message': 'Password does not meet the criteria' });
-            else {
+            if (!emailNotAvailable, !usernameNotAvailable, checkPw) {
                 await User.create(userData);
                 res.status(201).send({ 'message': 'User created' });
             }
         } catch (e) {
             console.log(e);
-            res.status(500).send({ 'Error': e, 'message': 'Service temporarily unavailable' });
+            res.status(400).send({ 'Error': e.message, 'message': 'All fields are required' });
         }
     },
 
@@ -42,9 +43,9 @@ export const authController = {
             const jwtPayload = { id: getUser._id, getRole };
             const token = jwt.sign(jwtPayload, process.env.JWT_SECRET);
 
-            res.status(200).json({ token });
+            if (getUser && checkPw) res.status(200).json({ token });
         } catch (e) {
-            res.status(500).send({ 'Error': e, 'message': 'Service temporarily unavailable' });
+            res.status(400).send({ 'Error': e.message, 'message': 'All fields are required' });
         }
     }
 }
