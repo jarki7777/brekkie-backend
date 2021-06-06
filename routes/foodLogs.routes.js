05abbc15c9720246b989d133d54ba0059c174f9f
TheJarki <melendez.jarki@gmail.com>
2021-06-06 14:55:18 +0200

    Update foodLogs.routes.js

--- routes/foodLogs.routes.js
+++ routes/foodLogs.routes.js
@@ -6,7 +6,7 @@ const foodLogRoutes = Router();
 
 foodLogRoutes.post('/:id', checkJwt, foodLogsController.add);
 foodLogRoutes.get('/find/', checkJwt, foodLogsController.showByDay);
-foodLogRoutes.get('/:id', checkJwt, foodLogsController.index);
+foodLogRoutes.get('/', checkJwt, foodLogsController.index);
 foodLogRoutes.patch('/:id', checkJwt, foodLogsController.addServing);
 
 export default foodLogRoutes;
